d849d40b744296690d091d118f534a5363f0cf4b
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-10 23:32:25 +0000

    Add files via upload
    fibonacci with recursive function

---
+++ fibonnaci recursion_with dict.py
@@ -0,0 +1,18 @@
+#utilazation of memoization concept
+import time
+def fib(n):
+    if n == 0:
+        return 0
+    
+    elif n== 1 :
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        ans = fib(n-1)+ fib(n-2)
+        return ans
+import time
+time1= time.time()
+print(fib(4))
+print(time.time()-time1)
+
