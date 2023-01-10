a7a903d7fe21ddc1a65944e38824912e693466fe
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-10 23:43:47 +0000

    Add files via upload

--- fibonnaci recursion_with dict.py
+++ fibonnaci recursion_with dict.py
@@ -1,18 +1,16 @@
-#finding fibonacci numbers using recursive function
+#utilazation of memoization concept
 import time
-def fib(n):
-    if n == 0:
-        return 0
+def fib(n, d = {0:0, 1:1, 2:1}):
+
     
-    elif n== 1 :
-        return 1
-    elif n == 2:
-        return 1
+    if n in d:
+        return d[n]
     else:
-        ans = fib(n-1)+ fib(n-2)
+        ans = fib(n-1,d)+ fib(n-2,d)
+        d[n] = ans
         return ans
 import time
 time1= time.time()
-print(fib(4))
+print(fib(1000))
 print(time.time()-time1)
 
